3cfc583fe59ee0566956a805e27d7bb8d3808922
komolei <944545149@qq.com>
2017-06-22 09:07:56 +0800

    addfile

--- resumer/config/index.js
+++ resumer/config/index.js
@@ -8,7 +8,7 @@ module.exports = {
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
         // assetsPublicPath: '/',
-        assetsPublicPath: '/jrg-project-5/resumer/dist',
+        assetsPublicPath: '/vue/resumer/dist',
         productionSourceMap: true,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
