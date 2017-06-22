21170bd5ab1767062396747c6aeb680214d75dd9
komolei <944545149@qq.com>
2017-06-22 15:44:59 +0800

    完善功能

--- resumer/src/components/ResumerEditor.vue
+++ resumer/src/components/ResumerEditor.vue
@@ -20,7 +20,7 @@
         </nav>
         <ol class="panels">
             <!--<li>1</li>
-                                                <li>2</li>
+                                                < li>2</li>
                                                 <li>3</li>
                                                 <li>4</li>
                                                 <li>5</li>
